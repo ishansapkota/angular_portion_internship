d9eac0a59ab3885ecfcc7392487e2a2e92a6c830
PERCOIDIT <ishansapkota2@gmail.com>
2024-07-03 17:32:12 +0545

    learned about input decorators and output decorators

--- first-ng-app(first day)/src/app/app.component.ts
+++ first-ng-app(first day)/src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { CommonModule } from '@angular/common';
+import { User } from '../models/user';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,UserProfileComponent],
+  imports: [RouterOutlet,UserProfileComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-ng-app';
+
+  users=[
+  {name:"Kishan",age:23,salary:100000,isMarried:false},
+  {name:"Nishan",age:23,salary:20000,isMarried:true},
+  {name:"SUdip",age:25,salary:10000,isMarried:false}
+  ]
+
+  receivedData(e:User)
+  {
+    console.log(e)
+  }
 }
--- first-ng-app(first day)/src/app/components/user-profile/user-profile.component.ts
+++ first-ng-app(first day)/src/app/components/user-profile/user-profile.component.ts
@@ -1,6 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, booleanAttribute, numberAttribute } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { User } from '../../../models/user';
+
+function addingHiToName(value:string)
+{
+  return "Hi "+value;
+}
+
 
 @Component({
   selector: 'app-user-profile',
@@ -10,22 +17,41 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
-  name = "Ishan Raj Sapkota"
-  age:number = 23 //explicitly defining the datatype
-  salary = 1000 
-  btnDisabled = false
 
-  onChange(e:Event)
-  {
-    let value = (e.target as HTMLInputElement).value
-    this.name = value
-    console.log(value)
-  }
+  //parent to child data passing
+  @Input({alias:"username",transform:addingHiToName}) name =""
+  @Input({transform:booleanAttribute}) isSingle!:boolean  //the ! sign denotes to the compiler that the variable is not null and some 
+                              // values will be assigned to it
+  @Input({transform:numberAttribute}) salary!:number // the transform function parses the inputted value which
+                                                    // might be in another form into the required form
+  
+  
+  
+  // name = "Ishan Raj Sapkota"
+  // age:number = 23 //explicitly defining the datatype
+  // salary = 1000 
+  // btnDisabled = false
 
-  users = [
-    {name:"Ishan",age:23,salary:10000,isMarried:false},
-    {name:"Kishan",age:23,salary:100000,isMarried:false},
-    {name:"Nishan",age:23,salary:20000,isMarried:true},
-    {name:"SUdip",age:25,salary:10000,isMarried:false}
-  ]
+  // onChange(e:Event)
+  // {
+  //   let value = (e.target as HTMLInputElement).value
+  //   this.name = value 
+  //   console.log(value)
+  // }
+
+  // users = [
+  //   {name:"Ishan",age:23,salary:10000,isMarried:false},
+  //   {name:"Kishan",age:23,salary:100000,isMarried:false},
+  //   {name:"Nishan",age:23,salary:20000,isMarried:true},
+  //   {name:"SUdip",age:25,salary:10000,isMarried:false}
+  // ]
+  
+  //Child to parent data passing
+
+  @Output() myEvent = new EventEmitter<User>()
+sendData()
+{
+  this.myEvent.emit({name:this.name,newsalary:20000})
+}
+ 
 }
